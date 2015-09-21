9d407e50cf6151d2287143936390e400628c711d
Technowhale <technowhalesolutions@gmail.com>
2015-09-21 12:42:26 +0100

    Update led.sh

--- led.sh
+++ led.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
 	gpio -g write 17 1
+ gpio -g write 27 0
+
+	gpio -g write 17 0
  gpio -g write 27 1
+
+	gpio -g write 17 1
+ gpio -g write 27 1
+
+	gpio -g write 17 1
+ gpio -g write 27 0
+
+
+	gpio -g write 17 1
+ gpio -g write 27 0
